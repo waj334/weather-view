b5923ab58e90d2ac9d6c5e62f4c87093eb25b93e
unknown <waj334@gmail.com>
2017-09-13 17:43:12 -0500

    Nearly complete interface
    Needs styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,123 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Segment, Container, Header, Divider, Card, Image} from 'semantic-ui-react';
+import {Segment, Container, Header, Divider, Card, Image, Grid} from 'semantic-ui-react';
 import * as weather from './weather.jsx';
+import Async from 'react-promise'
 
 class App extends Component {
-  render() {
-    var weekly = weather.apiGetWeatherWeek();
-    function forecastCard(forecast){
-      return (
-        <Card>
-          <Card.Header>
-            {weather.convertDay(forecast.day)}
-          </Card.Header>
-          <Card.Content>
-          </Card.Content>
-          <Card.Content extra>
-            {`H:${forcast.high}`}
-            <Divider vertical/>
-            {`L:${forecast.low}`}
-          </Card.Content>
-        </Card>
-      )
+  constructor(props) {
+    super(props);
+    this.state = {
+      forecast: weather.apiGetWeatherWeek(),
+      today: weather.apiGetWeatherToday()
     }
+  }
 
+  todayForecast(today){
+    return (
+      <Grid.Row columns='4'>
+        <Grid.Column width='2'>
+            <Header size='huge' textAlign='right'>Humidity</Header>
+            <Header size='huge' textAlign='right'>Dewpoint</Header>
+        </Grid.Column>
+
+        <Grid.Column width='1'>
+            <Header size='huge'>{today.hum+"%"}</Header>
+            <Header size='huge'>{today.dew}</Header>
+        </Grid.Column>
+
+        <Grid.Column>
+        </Grid.Column>
+
+        <Grid.Column>
+          <Header size='huge' as='h1' textAlign='center' style={{'font-size':'13em'}}>{today.temp}</Header>
+          <Grid columns='2'>
+            <Grid.Column>
+              <Container textAlign='left'>
+                <Header centered style={{'font-size':'3em'}}>
+                  {`H: ${today.h}`}
+                </Header>
+              </Container>
+            </Grid.Column>
+            <Grid.Column>
+              <Container textAlign='right'>
+                <Header centered style={{'font-size':'3em'}}>
+                  {`L: ${today.l}`}
+                </Header>
+              </Container>
+            </Grid.Column>
+          </Grid>
+        </Grid.Column>
+
+        <Grid.Column verticalAlign='middle' stretched>
+          <Image size='large' src={today.ico} style={{'width':'400px'}}/>
+        </Grid.Column>
+      </Grid.Row>
+    )
+  }
+  
+  forecastCard(forecast) {
+    return (
+      <Card fluid>
+        <Card.Header textAlign='center'>
+          <Segment basic>
+            <Header centered>
+              {forecast.day}
+            </Header>
+          </Segment>
+        </Card.Header>
+        <Card.Content textAlign='center'>
+          <Image fluid src={weather.apiGetWeatherIcon(forecast.ico)}/>
+        </Card.Content>
+        <Card.Content>
+          <Container>
+            {forecast.desc}
+          </Container>
+        </Card.Content>
+        <Card.Content extra>
+          <Grid columns='2'>
+            <Grid.Column>
+              <Container textAlign='left'>
+                <Header centered>
+                  {`H: ${forecast.high}`}
+                </Header>
+              </Container>
+            </Grid.Column>
+            <Grid.Column>
+              <Container textAlign='right'>
+                <Header centered>
+                  {`L: ${forecast.low}`}
+                </Header>
+              </Container>
+            </Grid.Column>
+          </Grid>
+        </Card.Content>
+      </Card>
+    )
+  }
+
+  render() {
     return (
       <div>
         <Segment.Group>
           <Segment basic>
+            <Header textAlign='left' size='huge'>Today</Header>
+            <Grid centered>
+              <Async promise={this.state.today} then={(today) => {return this.todayForecast(today)}}/>
+            </Grid>
           </Segment>
-          <Segment>
-            <Card.Group>
-
-            </Card.Group>
+          <Segment basic>
+            <Header textAlign='left' size='huge'>7 Day Forecast</Header>
+              <Async promise={this.state.forecast} then={(forecasts) => <Card.Group itemsPerRow='7'> {
+                  forecasts.map(function(forecast, i){
+                    return this.forecastCard(forecasts[i]);
+                  }, this)
+                }</Card.Group>} />
+            
           </Segment>
         </Segment.Group>
       </div>
-    );
-  }
+  )}
 }
 
 export default App;
--- src/weather.jsx
+++ src/weather.jsx
@@ -12,13 +12,27 @@ export var info = {
     'lng': '-85.6911'
 };
 
-export function apiGetWeatherToday() {
-    let url = `https://weather.weatherbug.com/api/observation?&lat=${info.lat}&lng=${info.lng}&timestamp=${Date.now()}`;
-    var data;
+export async function apiGetWeatherToday() {
+    let url = `https://weather.weatherbug.com/api/observation?lat=${info.lat}&lng=${info.lng}&providerId=3&stationId=NG044&timestamp=${Date.now()}`;
+    var data = {
+        //date: null,
+        temp: null,
+        h: null,
+        l: null,
+        ico: null,
+        desc: null,
+        hum: null,
+        dew: null
+    };
 
-    fetch(proxy+url).then(resp => resp.json())
-    .then(body => {
-        data = JSON.parse(body)
+    await fetch(proxy+url).then(resp => resp.json())
+    .then(json => {
+        data.temp = json.Observation.TemperatureDisplay;
+        data.h = json.HighLow.TemperatureHigh;
+        data.l = json.HighLow.TemperatureLow;
+        data.ico = apiGetWeatherIcon(json.Observation.IconCode);
+        data.hum = json.Observation.Humidity;
+        data.dew = json.Observation.DewPointDisplay;
     });
 
     return data;
@@ -41,10 +55,11 @@ export async function apiGetWeatherWeek() {
             var dt = new Date(Date.parse(w.Day.ForecastDateLocalStr));
 
             var cast = {
-                'day': convertDay(dt.getDay()),
-                'ico': w.Day.IconCode,
-                'high': w.Day.Temperature,
-                'low': w.Night.Temperature
+                day: convertDay(dt.getDay()),
+                ico: w.Day.IconCode,
+                high: w.Day.Temperature,
+                low: w.Night.Temperature,
+                desc: w.Day.DetailedDescription
             }
 
             processed.push(cast);
@@ -74,4 +89,11 @@ export function convertDay(d) {
         default:
             return 'INV';
     }
+}
+
+export function apiGetWeatherIcon(i) {
+    var token = 'c8ccaad9-43d9-4a3e-8ba9-78ac4aaf1b9f';
+    var href = `https://d3ltyiavrr2h27.cloudfront.net/resources/v1/resource/IconByCodeV1?iconset=forecast&iconSize=svglarge&iconCode=${i}&token=${token}`;
+
+    return href;
 }
